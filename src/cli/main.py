bdde95dfe04cf3a2d5010e778273ce209a7a6440
Lukas Keller <luk.keller94@gmail.com>
2024-09-22 21:43:33 +0200

    impl first methods

--- src/cli/main.py
+++ src/cli/main.py
@@ -1,5 +1,10 @@
 import typer
 from typing import Optional
+import duckdb
+from rich import print
+
+from .helpers import format_relation, OutputFormat
+
 
 app = typer.Typer(
     help="Parquet CLI tool based on DuckDb. Inspection, verification, and more."
@@ -8,10 +13,7 @@ app = typer.Typer(
 
 @app.command()
 def meta(
-    file: str,
-    json: Optional[bool] = typer.Option(
-        False, "--json", help="Output metadata in JSON format"
-    ),
+    file: str, format: OutputFormat = typer.Option("pretty", help="Output format")
 ):
     """
     Display basic metadata of the Parquet file without loading the schema.
@@ -24,6 +26,25 @@ def meta(
 
 @app.command()
 def head(
+    file: str,
+    records: int = typer.Option(10, help="Number of records to show"),
+    format: OutputFormat = typer.Option("pretty", help="Output format"),
+):
+    """
+    Display the first N records from the Parquet file.
+
+    :param file: Path to the Parquet file.
+    :param records: Number of records to display (default: 10).
+    :param as_csv: Output as CSV format.
+    """
+    # Load Parquet file and apply the limit using DuckDB
+    query = f"SELECT * FROM read_parquet('{file}') LIMIT {records};"
+    res = duckdb.query(query)  # not execute
+    typer.echo(format_relation(res, format))
+
+
+@app.command()
+def head_old(
     file: str,
     records: Optional[int] = typer.Option(
         10, "--records", help="Number of records to show"
@@ -59,17 +80,30 @@ def stats(
 @app.command()
 def cat(
     file: str,
-    json: Optional[bool] = typer.Option(
-        False, "--json", help="Output data in JSON format"
+    tolerate_large_file: Optional[bool] = typer.Option(
+        False, "--tolerate-large-file", help="Also cat files with >1M rows"
     ),
 ):
     """
-    Concatenate and display all records in the Parquet file.
+    Cat the Parquet file to the console, in CSV format.
 
     :param file: Path to the Parquet file.
-    :param json: Output in JSON format.
+    :param tolerate_large_file: Also cat files with >1M rows.
     """
-    typer.echo(f"Displaying all records for {file} (JSON: {json})")
+    if not tolerate_large_file:
+        # check row count
+        res = duckdb.query(
+            f"SELECT COUNT(*) as n_rows FROM read_parquet('{file}')"
+        ).fetchall()
+        if res[0][0] > 1_000_000:
+            typer.echo(
+                f"File {file} has more than 1M rows. If you are sure use --tolerate-large-file to proceed."
+            )
+            raise typer.Exit(code=1)
+
+    # read up the whole file
+    res = duckdb.query(f"SELECT * FROM read_parquet('{file}');")
+    typer.echo(format_relation(res, OutputFormat.csv))
 
 
 @app.command()
